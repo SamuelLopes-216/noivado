cd50f9d6c85b854356fb4ff309f81a50a693276c
SamuelLopes-216 <samuellopesanacleto216@gmail.com>
2023-11-17 19:07:09 -0300

    up

--- src/Confirma.jsx
+++ src/Confirma.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function Confirma() {
     return(
         <div className='backconf'>
-            <a href='https://wa.me/qr/35OMQE6JDKHAM1' className='link' id='confirma'>confirma sua Presença AQUI</a>
+            <div className="center"><a href='https://wa.me/qr/35OMQE6JDKHAM1' className='link' id='confirma'>confirma sua Presença AQUI</a></div>
         </div>
     )
 }
