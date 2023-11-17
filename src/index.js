8723e9909942eba7f28cf3440603e6769ad70763
SamuelLopes-216 <samuellopesanacleto216@gmail.com>
2023-11-17 18:42:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home.jsx';
-import Confirma from './Confirma.jsx';
+import Confirma from './confirma.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
